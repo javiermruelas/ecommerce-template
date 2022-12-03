d9701c332d0d8a696779229e0e84562868dda0d6
javiermruelas <javiermruelas@gmail.com>
2022-12-03 15:24:18 -0800

    adding products page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 			<ul>
 				<li><a href="/">Home</a></li>
 				<li><a href="/about">About</a></li>
-				<li><a href="/about">Products</a></li>
+				<li><a href="/products">Products</a></li>
 			</ul>
 		</nav>
 	</svelte:fragment>
