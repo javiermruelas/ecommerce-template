7485e4974f8e8aafd781bbaf580b9a5df20c56f2
javiermruelas <javiermruelas@gmail.com>
2022-12-03 13:00:44 -0800

    configuring skeletonUI + TailwindCSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script>
+	import '../theme.postcss';
+	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
+
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+    <!-- Header -->
+    <svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<h1>ecommerce</h1>
+				<img src="logo.svg" height="10%" width="10%" alt="ecommerce template logo" />
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm" href="https://github.com/javiermruelas/ecommerce-template" target="_blank" rel="noreferrer">GitHub</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+    <!-- Sidebar -->
+    <svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+    <!-- Page Content Slot -->
+    <slot />
+</AppShell>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+
+	darkMode: 'class',
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require('@skeletonlabs/skeleton/tailwind/theme.cjs')]
 };
 
 module.exports = config;
