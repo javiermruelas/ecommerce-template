9ce5205f3f2aede84a23fb514745f9fa193cafa8
javiermruelas <javiermruelas@gmail.com>
2022-12-11 15:24:15 -0800

    moved buttons

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,13 +15,4 @@
 	<section class="card card-body">
 		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 	</section>
-	<hr />
-	<section class="flex space-x-2">
-		<a class="btn btn-filled-primary" href="https://kit.svelte.dev/" target="_blank" rel="noreferrer">SvelteKit</a>
-		<a class="btn btn-filled-accent" href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind</a>
-		<a class="btn btn-filled-tertiary" href="https://skeleton.dev" target="_blank" rel="noreferrer">Skeleton</a>
-		<a class="btn btn-filled-primary" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
-		<a class="btn btn-filled-accent" href="https://stripe.com/" target="_blank" rel="noreferrer">Stripe</a>
-		<a class="btn btn-filled-tertiary" href="https://supabase.com/" target="_blank" rel="noreferrer">Supabase</a>
-	</section>
 </div>
