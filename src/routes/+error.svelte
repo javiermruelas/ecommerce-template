5e8b37d4439351b8d4144d035fee1d6c53e8520f
javiermruelas <javiermruelas@gmail.com>
2022-12-18 00:06:16 -0800

    creating custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+{#if $page.error}
+<div class="container mx-auto p-8 space-y-8">
+	<h1>{$page.status}: {$page.error.message}</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<hr />
+	<section class="card card-body">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	</section>
+	<hr />
+</div>
+{/if}
