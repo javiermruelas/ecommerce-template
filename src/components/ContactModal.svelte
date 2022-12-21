28f29cd02d79cfa889e97ce3a408f5fc0912e825
javiermruelas <javiermruelas@gmail.com>
2022-12-21 13:45:51 -0800

    re-designed contact modal for consistency

--- src/components/ContactModal.svelte
+++ src/components/ContactModal.svelte
@@ -18,47 +18,53 @@
 		modalStore.close();
 	}
 	// Base Classes
-	const cBase = 'space-y-4';
-	const cForm = 'space-y-4 rounded-container-token';
+	const baseClasses = '';
+	const formClasses = 'rounded-container-token';
+	console.log(parent);
 </script>
 
-<!-- @component This example creates a simple form modal. -->
-
-<div class="modal-example-form {cBase}">
-	<!-- Enable for debugging: -->
+<div class="{baseClasses}">
+    <!-- Enable for debugging: -->
 	<!-- <pre>{JSON.stringify(formData, null, 2)}</pre> -->
-	<header>
+	<header class="mb-2">
 		{#if parent.slot}
         	<slot></slot>
     	{/if}
-		<h1 class="text-amber-500 mb-4">Contact</h1>
+		<h1 class="text-amber-500 mb-2">Contact</h1>
 		<p>
 			If you're ready to take the next step 
 			in setting up an ecommerce store for 
 			your business, I'd be happy to help you!
 		</p>
 	</header>
-	<form class="modal-form {cForm}">
-		<label>
-			<span>Name</span>
-			<input type="text" bind:value={formData.name} placeholder="Enter name..." />
-		</label>
-		<label>
-			<span>Phone Number</span>
-			<input type="tel" bind:value={formData.tel} placeholder="Enter phone..." />
-		</label>
-		<label>
-			<span>Email</span>
-			<input type="email" bind:value={formData.email} placeholder="Enter email address..." />
-		</label>
-		<label>
-			<span>Comments</span>
-			<textarea bind:value={formData.comments} placeholder="Do you have anything questions or concerns?"></textarea>
-		</label>
-	</form>
-	<!-- prettier-ignore -->
-	<footer class="modal-footer {parent.regionFooter}">
-        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
-        <button class="btn {parent.buttonPositive}" on:click={onFormSubmit}>Submit Form</button>
-    </footer>
+    <form class="{formClasses}">
+		<div class="mb-4">
+			<label for="name" class="block mb-2 text-sm font-medium dark:text-white">Name</label>
+			<input type="text" id="name" bind:value={formData.name} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-surface-700 dark:border-stone-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" required>
+		</div>
+		<div class="mb-4">
+			<label for="phone-number" class="block mb-2 text-sm font-medium dark:text-white">Phone Number</label>
+			<input type="tel" id="phone-number" bind:value={formData.tel} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-surface-700 dark:border-stone-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" required>
+		</div>
+		<div class="mb-4">
+			<label for="email" class="block mb-2 text-sm font-medium dark:text-white">Email</label>
+			<input type="email" id="email" bind:value={formData.email} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-surface-700 dark:border-stone-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" required>
+		</div>
+		<div class="mb-4">
+			<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
+			<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-surface-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a question, comment or concern..."></textarea>
+		</div>
+	  	<footer class="mb-4 {parent.regionFooter}">
+        	<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+        	<button class="btn {parent.buttonPositive}" on:click={onFormSubmit}>Submit</button>
+		</footer>
+    </form>
+    <hr />
+    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll never share your details. Read our <a href="/privacyPolicy" class="font-medium text-amber-500 hover:underline">Privacy Policy</a>.</p>
 </div>
+
+<style>
+  input,textarea {
+    outline-color: #F59F0B !important;
+  }
+</style>
