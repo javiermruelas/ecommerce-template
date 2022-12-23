195712e893237fcbcadae45293586d6ef76f85bf
javiermruelas <javiermruelas@gmail.com>
2022-12-23 15:42:57 -0800

    the checkbox is not required nor should there be a placeholder

--- src/routes/signIn/+page.svelte
+++ src/routes/signIn/+page.svelte
@@ -26,7 +26,7 @@
   <form on:submit|preventDefault={signIn}>
     <div class="mb-6">
       <label for="email" class="block mb-2 text-sm font-medium dark:text-white">Please enter your email</label>
-      <input bind:value={formData.email} type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-surface-700 dark:border-stone-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" placeholder="name@ecommerce.com" required>
+      <input bind:value={formData.email} type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-surface-700 dark:border-stone-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500" required>
     </div>
     <div class="mb-6">
       <label for="password" class="block mb-2 text-sm font-medium dark:text-white">Your password</label>
@@ -34,7 +34,7 @@
     </div>
     <div class="flex items-start mb-6">
       <div class="flex items-center h-5">
-        <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-amber-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-amber-500 dark:ring-offset-amber-800 accent-amber-500" required>
+        <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-amber-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-amber-500 dark:ring-offset-amber-800 accent-amber-500">
       </div>
       <label for="remember" class="ml-2 text-sm font-medium">Remember me</label>
     </div>
