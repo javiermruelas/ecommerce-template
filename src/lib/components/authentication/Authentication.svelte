df2d6ca4989569d8811a7e2ea889e7d2ba65ca5e
javiermruelas <javiermruelas@gmail.com>
2023-02-12 15:01:25 -0800

    added more reactive variables that all lead to whether the current form is complete or not

--- src/lib/components/authentication/Authentication.svelte
+++ src/lib/components/authentication/Authentication.svelte
@@ -10,15 +10,24 @@
     let authForm: AuthForm = AuthHelpers.generateAuthForm(authType);
     // these helper variables will be filled in by zod
     let authFeedback: AuthFeedback = AuthHelpers.generateAuthFeedback(authType);
+
+    // these reactive variables will keep track of our state and will be updated in real time based on zod validation
     // is the field active ? if so, is the feedback active? if so has it been visited (if not visited then form has not been interacted with)? if so is there an error message (once interacted with I will show an error if empty as well so something valid has to be here)? 
     $: nameFieldValid = !authForm.formData.name ? false : !authFeedback.nameFeedback ? false : !authFeedback.nameFeedback.visited ? false : !authFeedback.nameFeedback.message ? false : true;
     $: emailFieldValid = !authForm.formData.email ? false : !authFeedback.emailFeedback ? false : !authFeedback.emailFeedback.visited ? false : !authFeedback.emailFeedback.message ? false : true;
     $: passwordFieldValid = !authForm.formData.password ? false : !authFeedback.passwordFeedback ? false : !authFeedback.passwordFeedback.visited ? false : !authFeedback.passwordFeedback.message ? false : true;
     $: passwordConfirmationFieldValid = !authForm.formData.passwordConfirmation ? false : !authFeedback.passwordConfirmationFeedback ? false : !authFeedback.passwordConfirmationFeedback.visited ? false : !authFeedback.passwordConfirmationFeedback.message ? false : true;
-    $: formComplete = nameFieldValid && emailFieldValid && passwordFieldValid && passwordConfirmationFieldValid;
+    $: signUpFormComplete = authType === 'signUp' && nameFieldValid && emailFieldValid && passwordFieldValid && passwordConfirmationFieldValid;
+    $: signInFormComplete = authType === 'signIn' && emailFieldValid && passwordFieldValid;
+    $: passwordRecoveryFormComplete = authType === 'passwordRecovery' && emailFieldValid;
+    $: passwordResetFormComplete = authType === 'passwordReset' && passwordFieldValid && passwordConfirmationFieldValid;
+    $: formComplete = signUpFormComplete || signInFormComplete || passwordRecoveryFormComplete || passwordResetFormComplete;
     
 
-    // sign in does not need any validation, passwordRecovery just needs to ensure it's a valid email
+    // sign in and passwordRecovery need minor email validation
+    // name field should be a minimum of two characters and a maximum of 50 characters
+    // email field should be a valid looking email - z.string().email()
+    //
 
     function parseForm(authForm: AuthForm): void {
         console.log("authForm: ", authForm);
