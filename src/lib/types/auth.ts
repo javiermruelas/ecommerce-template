03cf05933cadfd02821b6a2c545611f6a9ec573c
javiermruelas <javiermruelas@gmail.com>
2023-02-13 22:25:31 -0800

    converted type to zod schema while still maintaining type functionality

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,16 +1,21 @@
+import { z } from "zod";
+
 export type Auth = 'signUp' | 'signIn' | 'passwordRecovery' | 'passwordReset';
 
 export type Toast = 'primary' | 'secondary' | 'tertiary' | 'warning' | 'success' | 'error';
 
-export type AuthForm = {
-    formTitle: string,
-    formData: {
-        name?: string,
-        email?: string,
-        password?: string,
-        passwordConfirmation?: string
-    }
-}
+export const authFormSchema = z.object({
+    formTitle: z.string(),
+    formData: z.object({
+        name: z.string().min(2).max(40),
+        email: z.string().email(),
+        password: z.string().min(6),
+        passwordConfirmation: z.string().min(6)
+    })
+});
+
+// "AuthForm" is generated (or inferred) from the Zod schema
+export type AuthForm = z.infer<typeof authFormSchema>;
 
 export type Feedback = {
     message: string,
